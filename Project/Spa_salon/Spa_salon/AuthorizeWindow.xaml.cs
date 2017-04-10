3cbc29f751715323c0574c2b45b6c3d7989f78b9
Dmytro Struk <mr.struk@list.ru>
2017-04-10 14:25:52 +0300

    Initial commit. Added project folder with UI elements.

---
+++ Project/Spa_salon/Spa_salon/AuthorizeWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spa_salon
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WorkWindow window = new WorkWindow();
+            window.Show();
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow window = new RegistrationWindow();
+            window.Show();
+            Close();
+        }
+    }
+}
---
+++ Project/Spa_salon/Spa_salon/Views/SalonsUserControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Spa_salon.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SalonsUserControl.xaml
+    /// </summary>
+    public partial class SalonsUserControl : UserControl
+    {
+        public SalonsUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var printWindow = new PrintWindow();
+            printWindow.Title = "Дохід spa-салону";
+            printWindow.Show();
+        }
+    }
+}
